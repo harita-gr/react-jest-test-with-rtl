6ca42c69b8b092854df85f4836d536a5ee8d17db
harita-gr <haritagr19@gmail.com>
2025-10-01 13:12:55 +0900

    added mock tests

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -2,11 +2,16 @@
 
 import React, { useState } from "react";
 
-function Button({ label }) {
+function Button({ label, onClick }) {
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
     setCount((prev) => prev + 1);
+
+    // If parent passed a function, call it too
+    if (onClick) {
+      onClick();
+    }
   };
 
   return (
--- src/component/Button.test.jsx
+++ src/component/Button.test.jsx
@@ -4,9 +4,11 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "./Button";
 
 describe("Button Component", () => {
+  const handleClick = jest.fn(); // mock function
+
   // Runs before each test
   beforeEach(() => {
-    render(<Button label="Click Me" />);
+    render(<Button label="Click Me" onClick={handleClick} />);
   });
 
   test("renders button with correct label", () => {
@@ -31,5 +33,13 @@ describe("Button Component", () => {
   test("Button matches snapshot", () => {
     const { asFragment } = render(<Button label="Click Me" />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("calls onClick handler when button is clicked", () => {
+    const button = screen.getByText("Click Me");
+    fireEvent.click(button);
+
+    // Assertions
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
